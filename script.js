c01db0ecd51ec0ae35b4c76c035da0fc82c81a9e
CyndeliaOkami <isaiah@ellishome.org>
2017-03-14 09:10:11 -0600

    Got rid of name/gender/value, added text box

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //This is where the script will go!
 function startGame() {
-    var parent1 = document.getElementById("name");
+    var parent1 = document.getElementById("text");
     var child1 = document.getElementById("title");
-    var parent2 = document.getElementById("inputs");
-    var child2a = document.getElementById("player");
+    var parent2 = document.getElementById("setup");
+    var child2a = document.getElementById("name");
     var child2b = document.getElementById("gender");
     var child2c = document.getElementById("difficulty");
     var parent3 = document.getElementById("button");
@@ -16,9 +16,12 @@ function startGame() {
     parent2.removeChild(child2b);
     parent2.removeChild(child2c);
     parent3.removeChild(child3);
-    var para = document.createElement("p");
-    var node = document.createTextNode(name+" "+gender+" "+difficulty);
-    para.appendChild(node);
-    element = document.getElementById("name");
-    element.appendChild(para);
+    var storyBox = document.getElementById("storyBox");
+    var storyTxt = document.getElementById("story");
+    var background = document.getElementById("bg");
+    var input;
+    storyBox.style = "background-color:black; width:375px; height:400px; position: fixed; top: 50%; left: 50%; margin-top:-212px; margin-left:-188px;";
+    background.background = "https://media.giphy.com/media/EFhS1y3NnZP4k/giphy.gif";
+    storyTxt.style = "color:lightgrey; padding-left:25px;";
+    storyTxt.innerHTML = "There was once a time, long, long ago...<br>Before Humans had taken the planet.<br> <br>The world had, rather, <i>two</i> dominating species, working in turn to keep everything in balance.";
 }
