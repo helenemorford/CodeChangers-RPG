9dc78339be945c9de78939b43cb7280a3abac57b
CyndeliaOkami <isaiah@ellishome.org>
2017-03-13 20:33:14 -0600

    Made it display the name, gender, and difficulty value

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//here is where our script will go!
+//This is where the script will go!
+function startGame() {
+    var parent1 = document.getElementById("name");
+    var child1 = document.getElementById("title");
+    var parent2 = document.getElementById("inputs");
+    var child2a = document.getElementById("player");
+    var child2b = document.getElementById("gender");
+    var child2c = document.getElementById("difficulty");
+    var parent3 = document.getElementById("button");
+    var child3 = document.getElementById("start");
+    var name = child2a.value;
+    var gender = child2b.value;
+    var difficulty = child2c.value;
+    parent1.removeChild(child1);    
+    parent2.removeChild(child2a);
+    parent2.removeChild(child2b);
+    parent2.removeChild(child2c);
+    parent3.removeChild(child3);
+    var para = document.createElement("p");
+    var node = document.createTextNode(name+" "+gender+" "+difficulty);
+    para.appendChild(node);
+    element = document.getElementById("name");
+    element.appendChild(para);
+}
